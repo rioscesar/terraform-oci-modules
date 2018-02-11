1296e5003fd45475451124b7bb32257803519b9e
Cesar Rios <rios_cesar@utexas.edu>
2018-02-10 21:33:28 -0600

    Update nfs-client.sh

--- terraform-puppet/scripts/nfs-client.sh
+++ terraform-puppet/scripts/nfs-client.sh
@@ -4,7 +4,7 @@ sudo iptables -F
 
 sudo yum -y install nfs-utils
 
-# Firewall settings for NFS not needed for POC but reference
+# Firewall settings for NFS not needed for example but reference
 #sudo firewall-cmd --zone=public --add-port=2049/tcp --add-port=2049/udp --add-port=111/tcp --add-port=111/udp --add-port=32803/tcp --add-port=32769/udp --add-port=892/tcp --add-port=892/udp --add-port=662/tcp --add-port=662/udp
 #sudo firewall-cmd --permanent --zone=public --add-port=2049/tcp --add-port=2049/udp --add-port=111/tcp --add-port=111/udp --add-port=32803/tcp --add-port=32769/udp --add-port=892/tcp --add-port=892/udp --add-port=662/tcp --add-port=662/udp
 
